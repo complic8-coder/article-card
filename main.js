3fecfafa5981fbd0c51d3c3fb80969ea8f12d64a
complic8-coder <166186097+complic8-coder@users.noreply.github.com>
2024-04-16 14:21:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const shareIcon = document.querySelector('.share-icon path')
 const shareContent = document.querySelector('.share__content');
 
 shareBtn.addEventListener("click", () => {
-    if (shareContent.style.opacity === "0") {
-        shareContent.style.opacity = "1";
+    if (shareContent.style.visibility === "hidden") {
+        shareContent.style.visibility = "visible";
 
         shareBtn.style.background = "hsl(217, 19%, 35%)";
         shareIcon.setAttribute('fill', 'hsl(210, 46%, 95%)');
     } else {
-        shareContent.style.opacity = "0";
+        shareContent.style.visibility = "hidden";
 
         shareBtn.style.background = "hsl(210, 46%, 95%)";
         shareIcon.setAttribute('fill', '#6E8098');
